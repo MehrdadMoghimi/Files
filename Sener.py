7fde10b88e2b3a30b2d37378f770bff3fcddb448
mehrdad96 <mehrdad.m7496@gmail.com>
2020-04-24 23:34:24 +0430

    Add files via upload

--- Sener.py
+++ Sener.py
@@ -6,8 +6,8 @@ from statsmodels.tsa.arima_model import ARIMA
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from numpy.random import seed
 import sys
+import time
 
 
 def safe_space_PM(returns, VARs):
@@ -50,36 +50,29 @@ def HELP(returns):
     return value_at_risk
 
 
-def forecast_std(portfolio_returns, window_length, volatility_model='GARCH', dist='Normal'):
-    # volatility_model = {GARCH, EGARCH, ARCH, HARCH, GJR-GARCH, TARCH}
+def forecast_std(returns, weights, window_length, test_len, volatility_model='GARCH', dist='Normal'):
+    # volatility_model = {GARCH, EGARCH, GJR-GARCH}
     # dist = {Normal, t, skewt, ged}
+    portfolio_returns = returns.dot(weights)
     portfolio_returns = portfolio_returns * 100.0
-    test_len = portfolio_returns.shape[0] - window_length
     forecast_std_arch = np.zeros(test_len)
     forecast_mean_arch = np.zeros(test_len)
     print('\n' + volatility_model + ':')
     if volatility_model == 'GJR-GARCH':
         for j in range(0, test_len):
+            loc = returns.shape[0] - test_len + j
             progressBar(j, test_len, bar_length=20)
-            window = portfolio_returns[j:j + window_length]
+            window = portfolio_returns.iloc[loc - window_length:loc]
             arch = arch_model(window, p=1, o=1, q=1)
-            arch_fit = arch.fit(disp="off")
-            arch_forecast = arch_fit.forecast(horizon=1)
-            forecast_mean_arch[j] = arch_forecast.mean.iloc[-1, 0]
-            forecast_std_arch[j] = np.sqrt(arch_forecast.variance.iloc[-1, 0])
-    elif volatility_model == 'TARCH':
-        for j in range(0, test_len):
-            progressBar(j, test_len, bar_length=20)
-            window = portfolio_returns[j:j + window_length]
-            arch = arch_model(window, p=1, o=1, q=1, power=1.0)
             arch_fit = arch.fit(disp="off")
             arch_forecast = arch_fit.forecast(horizon=1)
             forecast_mean_arch[j] = arch_forecast.mean.iloc[-1, 0]
             forecast_std_arch[j] = np.sqrt(arch_forecast.variance.iloc[-1, 0])
     else:
         for j in range(0, test_len):
+            loc = returns.shape[0] - test_len + j
             progressBar(j, test_len, bar_length=20)
-            window = portfolio_returns[j:j + window_length]
+            window = portfolio_returns.iloc[loc - window_length:loc]
             arch = arch_model(window, mean='AR', dist=dist, vol=volatility_model, p=1, q=1)
             arch_fit = arch.fit(disp="off")
             arch_forecast = arch_fit.forecast(horizon=1)
@@ -106,105 +99,101 @@ def forecast_mean(portfolio_returns, window_length):
     return forecast
 
 
-def calculate_Var_Covar_VAR(portfolio_returns, window_length, alpha):
-    test_len = portfolio_returns.shape[0] - window_length
-    forecast_std_n = np.zeros(test_len)
+def calculate_Var_Covar_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
+    std_VaR = np.zeros(test_len)
     print('\nVar Covar:')
     for j in range(0, test_len):
+        loc = returns.shape[0] - test_len + j
         progressBar(j, test_len, bar_length=20)
-        window = portfolio_returns[j:j + window_length]
-        forecast_std_n[j] = np.std(window)
-    std_VaR = norm.ppf(alpha / 100.0) * forecast_std_n
+        window = returns.iloc[loc - window_length:loc, :]
+        cov_matrix = window.cov()
+        avg_rets = window.mean()
+        port_mean = avg_rets.dot(weights)
+        port_stdev = np.sqrt(weights.T.dot(cov_matrix).dot(weights))
+        std_VaR[j] = norm.ppf(alpha / 100.0, port_mean, port_stdev)
+    print('\nVar_Covar time: {}'.format(round(time.time() - t, 2)))
     return std_VaR
 
 
-def calculate_RiskMetrics_VAR(portfolio_returns, window_length, alpha):
+def calculate_RiskMetrics_VAR(returns, weights, test_len, alpha):
+    t = time.time()
     lambda_risk_metric = 0.94
-    forecast_std_risk_metric = pd.Series(portfolio_returns.squeeze()).ewm(alpha=1 - lambda_risk_metric).std().shift(
-        periods=1)
-    forecast_std_test = forecast_std_risk_metric[window_length:]
+    portfolio_returns = returns.dot(weights)
+    forecast_std_risk_metric = pd.Series(portfolio_returns.squeeze()).ewm(alpha=1 - lambda_risk_metric).std().shift(periods=1)
+    forecast_std_test = forecast_std_risk_metric[-test_len:]
     risk_metric_VaR = norm.ppf(alpha / 100.0) * forecast_std_test
+    print('\nRiskMetrics time: {}'.format(round(time.time() - t, 2)))
     return risk_metric_VaR
 
 
-def calculate_GARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_garch, forecast_std_garch = forecast_std(portfolio_returns, window_length, volatility_model='GARCH')
+def calculate_GARCH_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
+    forecast_mean_garch, forecast_std_garch = forecast_std(returns, weights, window_length, test_len, volatility_model='GARCH')
     garch_VaR = forecast_mean_garch + norm.ppf(alpha / 100.0) * forecast_std_garch
+    print('\nGARCH time: {}'.format(round(time.time() - t, 2)))
     return garch_VaR
 
 
-def calculate_FIGARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_figarch, forecast_std_figarch = forecast_std(portfolio_returns, window_length,
-                                                               volatility_model='FIGARCH')
-    garch_VaR = forecast_mean_figarch + norm.ppf(alpha / 100.0) * forecast_std_figarch
-    return garch_VaR
-
-
-def calculate_EGARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_egarch, forecast_std_egarch = forecast_std(portfolio_returns, window_length,
-                                                             volatility_model='EGARCH')
+def calculate_EGARCH_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
+    forecast_mean_egarch, forecast_std_egarch = forecast_std(returns, weights, window_length, test_len, volatility_model='EGARCH')
     egarch_VaR = forecast_mean_egarch + norm.ppf(alpha / 100.0) * forecast_std_egarch
+    print('\nEGARCH time: {}'.format(round(time.time() - t, 2)))
     return egarch_VaR
 
 
-def calculate_ARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_arch, forecast_std_arch = forecast_std(portfolio_returns, window_length, volatility_model='ARCH')
-    fiarch_VaR = forecast_mean_arch + norm.ppf(alpha / 100.0) * forecast_std_arch
-    return fiarch_VaR
-
-
-def calculate_HARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_harch, forecast_std_harch = forecast_std(portfolio_returns, window_length, volatility_model='HARCH')
-    harch_VaR = forecast_mean_harch + norm.ppf(alpha / 100.0) * forecast_std_harch
-    return harch_VaR
-
-
-def calculate_TARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_tarch, forecast_std_tarch = forecast_std(portfolio_returns, window_length, volatility_model='TARCH')
+def calculate_GJR_GARCH_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
+    forecast_mean_tarch, forecast_std_tarch = forecast_std(returns, weights, window_length, test_len, volatility_model='GJR-GARCH')
     harch_VaR = forecast_mean_tarch + norm.ppf(alpha / 100.0) * forecast_std_tarch
+    print('\nGJR GARCH time: {}'.format(round(time.time() - t, 2)))
     return harch_VaR
 
 
-def calculate_GJR_GARCH_VAR(portfolio_returns, window_length, alpha):
-    forecast_mean_tarch, forecast_std_tarch = forecast_std(portfolio_returns, window_length,
-                                                           volatility_model='GJR-GARCH')
-    harch_VaR = forecast_mean_tarch + norm.ppf(alpha / 100.0) * forecast_std_tarch
-    return harch_VaR
-
-
-def calculate_Historical_VAR(portfolio_returns, window_length, alpha):
-    test_len = portfolio_returns.shape[0] - window_length
+def calculate_Historical_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
     hist_VaR = np.zeros((test_len, 1))
+    portfolio_returns = returns.dot(weights)
     print('\nHistorical:')
     for j in range(test_len):
         progressBar(j, test_len, bar_length=20)
         window = portfolio_returns[j:j + window_length]
         hist_VaR[j] = np.percentile(window, alpha)
+    print('\nHistorical time: {}'.format(round(time.time() - t, 2)))
     return hist_VaR
 
 
-def calculate_Filtered_Historical_VAR(portfolio_returns, window_length, alpha, forecast_mean_arima, forecast_std_garch):
-    test_len = portfolio_returns.shape[0] - window_length
+def calculate_Filtered_Historical_VAR(returns, weights, window_length, test_len, alpha, forecast_mean, forecast_std):
+    t = time.time()
     f_hist_VaR = np.zeros((test_len, 1))
+    portfolio_returns = returns.dot(weights)
     print('\nFiltered Historical:')
     for j in range(test_len):
+        loc = returns.shape[0] - test_len + j
         progressBar(j, test_len, bar_length=20)
-        window = portfolio_returns[j:j + window_length]
-        MEAN = forecast_mean_arima[j]
-        STD = forecast_std_garch[j]
+        window = portfolio_returns.iloc[loc - window_length:loc]
+        MEAN = forecast_mean[j]
+        STD = forecast_std[j]
         filtered_window = MEAN + (window - np.mean(window)) * (STD / np.std(window))
         f_hist_VaR[j] = np.percentile(filtered_window, alpha)
+    print('\nFiltered Historical time: {}'.format(round(time.time() - t, 2)))
     return f_hist_VaR
 
 
-def calculate_MonteCarlo_VAR(alpha, forecast_mean_arima, forecast_std_garch):
-    seed(0)
+def calculate_MonteCarlo_VAR(returns, weights, window_length, test_len, alpha):
+    t = time.time()
     n_samples = 10000
-    STD = forecast_std_garch.reshape((-1, 1))
-    MEAN = forecast_mean_arima.reshape((-1, 1))
-    raw_samples = np.random.normal(0, 1, size=(1, n_samples))
-    samples_with_scale = np.repeat(MEAN, n_samples, axis=1) + np.dot(STD, raw_samples)
-    mc_VaR = np.percentile(samples_with_scale, q=alpha, axis=1).reshape(-1, 1)
+    mc_VaR = np.zeros(test_len)
+    print('\nMonte Carlo:')
+    for j in range(0, test_len):
+        loc = returns.shape[0] - test_len + j
+        progressBar(j, test_len, bar_length=20)
+        window = returns.iloc[loc - window_length:loc, :]
+        log_return = np.random.multivariate_normal(window.mean() - (window.std() ** 2) / 2, window.cov(), size=n_samples)
+        port_simulations = (np.exp(log_return) - 1).dot(weights)
+        mc_VaR[j] = np.percentile(port_simulations, alpha)
+    print('\nMonte Carlo time: {}'.format(round(time.time() - t, 2)))
     return mc_VaR
 
 
@@ -228,31 +217,25 @@ def calculate_EVT_VAR():
     return 0
 
 
-def calculate_var_models(portfolio_returns, window_length, alpha, forecast_mean_arima, forecast_std_garch):
+def calculate_var_models(returns, weights, window_length, test_len, alpha, forecast_mean, forecast_std):
     var_models = pd.DataFrame()
     # var_models['CAViaR_Sym'] = calculate_CAViaR_Sym_VAR(portfolio_returns, window_length, alpha)
     # var_models['CAViaR_Asym'] = calculate_CAViaR_Asym_VAR(portfolio_returns, window_length, alpha)
     # var_models['CAViaR_indirect_GARCH'] = calculate_CAViaR_indirect_GARCH_VAR(portfolio_returns, window_length, alpha)
     # var_models['CAViaR_adaptive'] = calculate_CAViaR_adaptive_VAR(portfolio_returns, window_length, alpha)
-    # var_models['Var_Covar'] = calculate_Var_Covar_VAR(portfolio_returns, window_length, alpha)
-    var_models['RiskMetrics'] = calculate_RiskMetrics_VAR(portfolio_returns, window_length, alpha)
-    var_models['Historical'] = calculate_Historical_VAR(portfolio_returns, window_length, alpha)
-    var_models['F_Historical'] = calculate_Filtered_Historical_VAR(portfolio_returns, window_length, alpha,
-                                                                   forecast_mean_arima, forecast_std_garch)
-    var_models['MonteCarlo'] = calculate_MonteCarlo_VAR(alpha, forecast_mean_arima, forecast_std_garch)
-    var_models['GARCH'] = calculate_GARCH_VAR(portfolio_returns, window_length, alpha)
-    var_models['EGARCH'] = calculate_EGARCH_VAR(portfolio_returns, window_length, alpha)
-    # var_models['FIGARCH'] = calculate_FIGARCH_VAR(portfolio_returns, window_length, alpha)
-    # var_models['ARCH'] = calculate_ARCH_VAR(portfolio_returns, window_length, alpha)
-    # var_models['HARCH'] = calculate_HARCH_VAR(portfolio_returns, window_length, alpha)
-    # var_models['TARCH'] = calculate_TARCH_VAR(portfolio_returns, window_length, alpha)
+    var_models['RiskMetrics'] = calculate_RiskMetrics_VAR(returns, weights, test_len, alpha)
+    var_models['Var_Covar'] = calculate_Var_Covar_VAR(returns, weights, window_length, test_len, alpha)
+    # var_models['Historical'] = calculate_Historical_VAR(returns, weights, window_length, test_len, alpha)
+    var_models['F_Historical'] = calculate_Filtered_Historical_VAR(returns, weights, window_length, test_len, alpha, forecast_mean, forecast_std)
+    var_models['MonteCarlo'] = calculate_MonteCarlo_VAR(returns, weights, window_length, test_len, alpha)
+    var_models['GARCH'] = calculate_GARCH_VAR(returns, weights, window_length, test_len, alpha)
+    var_models['EGARCH'] = calculate_EGARCH_VAR(returns, weights, window_length, test_len, alpha)
     # var_models['GJR_GARCH'] = calculate_GJR_GARCH_VAR(portfolio_returns, window_length, alpha)
     # var_models['EVT'] = calculate_EVT_VAR(portfolio_returns, window_length, alpha)
     return var_models
 
 
-def calculate_var_models_pm(portfolio_returns, window_length, var_models, alpha):
-    test_returns = portfolio_returns[window_length:]
+def calculate_var_models_pm(test_returns, var_models, alpha):
     var_models_pm = pd.DataFrame(columns=['name', 'PM', 'ratio'])
     for column in var_models.columns:
         var_models_pm = var_models_pm.append(
@@ -262,8 +245,7 @@ def calculate_var_models_pm(portfolio_returns, window_length, var_models, alpha)
     return var_models_pm
 
 
-def plot_all(portfolio_returns, window_length, var_models):
-    test_returns = portfolio_returns[window_length:]
+def plot_all(test_returns, var_models):
     for column in var_models.columns:
         plot(test_returns, var_models[column].values, file_name=column)
 
@@ -271,14 +253,27 @@ def plot_all(portfolio_returns, window_length, var_models):
 def predictive_ability_test(test_returns, var_models, alpha, loss_func):
     var_models_error = var_models.subtract(test_returns, axis=0)
     # loss is a function of errors, it can be abs or power of 2
-    if loss_func=='mse':
+    if loss_func == 'mse':
         var_models_loss = np.sqrt(np.power(var_models_error, 2))
-    elif loss_func=='abs':
+    elif loss_func == 'abs':
         var_models_loss = np.abs(var_models_error)
-    elif loss_func=='regulatory':   
+    elif loss_func == 'regulatory':
         var_models_loss = ((np.repeat(test_returns.values.reshape(-1, 1), var_models.shape[1], axis=1) < var_models) * 1 - alpha / 100) * var_models_error
+    elif loss_func == 'quantile':
+        var_models_loss = pd.DataFrame(index=var_models.index, columns=var_models.columns)
+        for column in var_models.columns:
+            var_model = var_models[column]
+            QL = []
+            for i in range(len(var_model) - 1):
+                if test_returns[i] < var_model[i]:
+                    QuantileLoss = (test_returns[i] - var_model[i]) ** 2
+                else:
+                    QuantileLoss = (test_returns[i + 1:].quantile(alpha / 100) - var_model[i]) ** 2
+                QL.append(QuantileLoss)
+            QL.append((test_returns[-1] - var_model[-1]) ** 2)
+            var_models_loss[column] = QL
     else:
-        return "loss function must be one of mse, abs or regulatory"
+        return "loss function must be one of quantile, mse, abs or regulatory"
     kappa = var_models_loss.div(np.sum(var_models_loss, axis=1), axis=0)
     W = np.sum(kappa > (1 / var_models_loss.shape[1]))
     p = 0.5
